579cc6e3165ccef88e4294b275ebaa00a590d6f0
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-03-09 00:47:12 +0100

    Added todo-note

--- NgCmsWeb/src/app/models/register.model.ts
+++ NgCmsWeb/src/app/models/register.model.ts
@@ -1,6 +1,7 @@
 import { Serializable } from './../interfaces/serializable.interface';
 
 export class Register implements Serializable<Register> {
+    // TODO: Change to camelcase
     Email: string;
     Password: string;
     ConfirmPassword: string;
