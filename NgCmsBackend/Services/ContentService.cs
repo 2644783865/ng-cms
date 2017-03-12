98d885974d5ee97f82312174b248dafa7eb127b2
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-03-13 00:22:00 +0100

    Added service to handle content

---
+++ NgCmsBackend/Services/ContentService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using NgCmsBackend.Repositories;
+using System.Web;
+using NgCmsBackend.DbContexts;
+
+namespace NgCmsBackend.Services
+{
+    public class ContentService
+    {
+        public void Dispose()
+        {
+        }
+
+        private readonly BaseDbContext _dbContext = BaseDbContext.Create();
+
+        TblContentRepository ContentRepo => new TblContentRepository(_dbContext);
+
+        public async Task<tblContent> GetContentByName(string name)
+        {
+            return await ContentRepo.FindAsync(x => x.Name.Equals(name));
+        }
+
+        public async Task CreateContent(tblContent content)
+        {
+            await ContentRepo.Insert(content);
+        }
+    }
+}
