ccaf9c17d594b6ee1d2adfd1941943fca8c3ac50
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-03-13 00:20:51 +0100

    Added RoleListModel

---
+++ NgCmsApi/Models/RoleListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NgCmsApi.Models
+{
+    public class RoleListModel
+    {
+        [Required]
+        [JsonProperty("guid")]
+        public Guid Guid { get; set; }
+
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
