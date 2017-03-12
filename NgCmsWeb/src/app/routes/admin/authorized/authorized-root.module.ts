aec1f9f7230f8344c7d8cae0a37356d71c8e7bd5
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-03-13 00:22:46 +0100

    Added route in client

--- NgCmsWeb/src/app/routes/admin/authorized/authorized-root.module.ts
+++ NgCmsWeb/src/app/routes/admin/authorized/authorized-root.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AuthorizedRoot } from './authorized-root.component';
 import { MainModule } from './main/main.module';
+import { UsersModule } from './users/users.module';
+import { EditContentModule } from './edit-content/edit-content.module';
 import { AuthorizedRoutes } from './authorized-root.routes';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [CommonModule, MainModule, AuthorizedRoutes],
+    imports: [CommonModule, MainModule, UsersModule, EditContentModule, AuthorizedRoutes],
     exports: [AuthorizedRoot],
     declarations: [AuthorizedRoot]
 })
--- NgCmsWeb/src/app/routes/admin/authorized/authorized-root.routes.ts
+++ NgCmsWeb/src/app/routes/admin/authorized/authorized-root.routes.ts
@@ -2,12 +2,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthorizedRoot } from './authorized-root.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Main } from './main/main.component';
+import { Users } from './users/users.component';
+import { EditContent } from './edit-content/edit-content.component';
 
 export const routes: Routes = [
     {
         path: '', component: AuthorizedRoot, children: [
             { path: 'main', component: Main },
-            { path: '', redirectTo: 'main', pathMatch: 'full' }
+            { path: '', redirectTo: 'main', pathMatch: 'full' },
+            { path: 'users', component: Users },
+            { path: 'edit-content/:nc-name', component: EditContent }
         ]
     },
 ];
