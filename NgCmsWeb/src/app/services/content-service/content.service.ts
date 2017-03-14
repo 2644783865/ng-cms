44e32cbd6169024734f2b060f2d6475d3b0a6d54
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-03-14 23:12:56 +0100

    Reworked service for content based on new page-logic

--- NgCmsWeb/src/app/services/content-service/content.service.ts
+++ NgCmsWeb/src/app/services/content-service/content.service.ts
@@ -3,27 +3,62 @@ import { Router } from '@angular/router';
 import { Headers } from '@angular/http';
 import { InterceptorService } from './../interceptor-service/interceptor.service';
 import { Observable } from 'rxjs/Observable';
+import { Content } from './../../models/content.model';
+
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class ContentService {
-
-    private _baseUrl: string;
+    public contentArr: Content[] = [];
+    public contentLoaded: boolean = false;
+    private baseUrl: string;
     constructor(private interceptor: InterceptorService, private router: Router) {
-        this._baseUrl = 'api/Content/';
+        this.baseUrl = 'api/Content/';
     }
 
-    public getContentByName(name) {
-        return this.interceptor.get(this._baseUrl + 'GetByName/' + name).map(res => {
+    public getContentForPage(page) {
+        this.contentLoaded = false;
+        this.getContentByPage(page).subscribe(res => {
+            this.contentArr = res;
+            this.contentLoaded = true;
+        });
+    }
+
+    public getContent() {
+        return this.interceptor.get(this.baseUrl).map(res => {
             return res;
         }).catch(error => {
             return Observable.of(error);
         });
     }
 
-    public createContent(content) {
-        return this.interceptor.post(this._baseUrl + 'Create', JSON.stringify(content));
+    public getContentByName(name) {
+        return this.interceptor.get(this.baseUrl + 'GetByName/' + name).map(res => {
+            return res;
+        }).catch(error => {
+            return Observable.of(error);
+        });
+    }
+
+    public getContentByPage(guid) {
+        return this.interceptor.get(this.baseUrl + 'GetByPage/' + guid).map(res => {
+            return res;
+        }).catch(error => {
+            return Observable.of(error);
+        });
+    }
+
+    public getContentByPaths(model) {
+        return this.interceptor.post(this.baseUrl + 'GetByPaths', JSON.stringify(model));
+    }
+
+    public createContent(model) {
+        return this.interceptor.post(this.baseUrl + 'Create', JSON.stringify(model));
+    }
+
+    public updateContent(model) {
+        return this.interceptor.post(this.baseUrl + 'Update', JSON.stringify(model));
     }
 }
 
