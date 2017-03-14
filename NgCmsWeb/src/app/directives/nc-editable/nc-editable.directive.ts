fd0a0d4f7281e7176164828574e7c70a47c84f0f
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-03-14 23:09:56 +0100

    Reworked directive to support pages

--- NgCmsWeb/src/app/directives/nc-editable/nc-editable.directive.ts
+++ NgCmsWeb/src/app/directives/nc-editable/nc-editable.directive.ts
@@ -2,6 +2,8 @@ import { Directive, ElementRef, Input } from '@angular/core';
 import { ContentService } from './../../services/content-service/content.service';
 import { ContentCreate } from './../../models/content-create.model';
 import { Content } from './../../models/content.model';
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth-service/auth.service';
 
 @Directive({ selector: '[ncEditable]' })
 
@@ -10,34 +12,55 @@ export class NcEditableDirective {
     editLink: string;
     contentId: string;
     content: Content;
+    foundContent: Content;
+    constructor(private element: ElementRef, private contentService: ContentService, private router: Router, private authService: AuthService) {
 
-    constructor(private element: ElementRef, private contentService: ContentService) {
     }
 
     ngOnInit() {
-        this.contentService.getContentByName(this.ncName).subscribe(res => {
-            if (res === null) {
+        this.foundContent = this.contentService.contentArr.find(c => c.name == this.ncName);
+        this.initContent();
+    }
+
+    initContent() {
+        if (this.authService.isLoggedIn()) {
+            if (this.foundContent === undefined) {
+                debugger;
                 const content = new ContentCreate().deserialize({
                     name: this.ncName,
-                    content: this.element.nativeElement.innerHTML
+                    content: this.element.nativeElement.innerHTML,
+                    path: this.router.url
                 });
                 this.contentService.createContent(content).subscribe(res => {
                     this.content = new Content().deserialize(content);
                     this.content.guid = res;
-                    this.setContent();
+                    this.appendLink();
                 });
             } else {
-                this.content = res
-                this.setContent();
+                this.content = this.foundContent;
+                this.appendLink();
             }
-        });
+        } else {
+            this.element.nativeElement.innerHTML = this.foundContent.content;
+        }
     }
 
-    setContent() {
-        this.editLink = 'admin/edit-content/' + this.content.guid;
+    appendLink() {
+        this.editLink = '/admin/edit-content/' + this.content.guid;
         this.element.nativeElement.innerHTML =
-            '<a class="edit-link" href="' + this.editLink + '">' +
+            '<a class="edit-link" href="' + this.editLink + '" id="' + this.content.guid + '">' +
             '<i class="fa fa-pencil" style="padding-right: 5px;" aria-hidden="true"></i></a>' +
             this.content.content;
+
+        $('#' + this.content.guid).click(e => {
+            e.preventDefault();
+            this.router.navigateByUrl($('#' + this.content.guid).attr('href'));
+        });
+
+        const foundContent = this.contentService.contentArr.find(c => c.guid === this.content.guid);
+
+        if (foundContent === undefined) {
+            this.contentService.contentArr.push(this.content);
+        }
     }
 }
